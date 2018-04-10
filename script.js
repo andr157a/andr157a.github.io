fbed68c1d715e07891c73246d4d6de83242d987a
andr157a <37779693+andr157a@users.noreply.github.com>
2018-04-10 21:41:20 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var date = new Date();
-
-var datemon = date.getDate();
-var month = date.getMonth() + 1;
-var year = date.getFullYear();
-
-document.write("<p class='leftsidemenu'>Current date: " + datemon + "/" + month + " " +
-    year + "</p>");
